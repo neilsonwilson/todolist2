5cd7b7fa97d2149e64145a9c317b16a14e512d20
Neil <macadamiannut@gmail.com>
2014-11-01 15:31:32 -0700

    created resources for tasks, deleted all views except for index

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  resources :tasks, only: [:index, :new, :create, :update, :destroy]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
